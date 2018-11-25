95f8b8de8bd21180f8d8316df79bf3bdf4438d15
Tatyabhau R. Chavan <tatyabhau.chavan@gmail.com>
2018-11-25 12:26:27 +0530

    new changes

---
+++ app/src/main/java/com/passion/hindilovesms/SmsDetailActivity.kt
@@ -0,0 +1,20 @@
+package com.passion.hindilovesms
+
+import android.os.Bundle
+import android.support.design.widget.Snackbar
+import android.support.v7.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_sms_detail.*
+import kotlinx.android.synthetic.main.content_sms_detail.*
+
+class SmsDetailActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sms_detail)
+        setSupportActionBar(toolbar)
+        lblMessage.text = intent.extras.get("message").toString()
+        fab.setOnClickListener {
+
+        }
+    }
+}
--- app/src/main/java/com/passion/hindilovesms/adapter/MessagesAdapter.kt
+++ app/src/main/java/com/passion/hindilovesms/adapter/MessagesAdapter.kt
@@ -1,11 +1,20 @@
 package com.passion.hindilovesms.adapter
 
+import android.content.Intent
 import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.passion.hindilovesms.R
+import com.passion.hindilovesms.SmsDetailActivity
 import kotlinx.android.synthetic.main.layout_message_row.view.*
+import android.content.ClipData
+import android.content.ClipboardManager
+import android.content.Context.CLIPBOARD_SERVICE
+import android.support.v4.content.ContextCompat.getSystemService
+import android.support.v4.content.ContextCompat.startActivity
+import android.widget.Toast
+
 
 class MessagesAdapter(var messages: List<String>) : RecyclerView.Adapter<MessagesAdapter.MessagesViewHolder>() {
 
@@ -29,12 +38,25 @@ class MessagesAdapter(var messages: List<String>) : RecyclerView.Adapter<Message
         viewHolder.lblMessage.text = lstMessages.get(position)
 
         viewHolder.imgCopy.setOnClickListener {
-
+            val clipboard = it.context.getSystemService(CLIPBOARD_SERVICE) as ClipboardManager?
+            val clip = ClipData.newPlainText("label", lstMessages.get(position))
+            clipboard!!.primaryClip = clip
+            Toast.makeText(it.context, "Copied", Toast.LENGTH_LONG).show()
         }
 
         viewHolder.imgShare.setOnClickListener {
-
+            val sharingIntent = Intent(android.content.Intent.ACTION_SEND)
+            sharingIntent.type = "text/plain"
+            sharingIntent.putExtra(android.content.Intent.EXTRA_TEXT, lstMessages.get(position))
+            it.context.startActivity(Intent.createChooser(sharingIntent, "Share via"))
         }
+
+        viewHolder.cvContainder.setOnClickListener {
+            val intent = Intent(it.context, SmsDetailActivity::class.java)
+            intent.putExtra("message", lstMessages.get(position))
+            it.context.startActivity(intent)
+        }
+
 
     }
 
@@ -43,6 +65,7 @@ class MessagesAdapter(var messages: List<String>) : RecyclerView.Adapter<Message
         val lblMessage = view.lblName
         val imgCopy = view.imgCopy
         val imgShare = view.imgShare
+        val cvContainder = view.cvContainer
     }
 
 }
